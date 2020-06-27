2b99ff77b3071f41ead5577060f09a7bf34a61d1
Weilando <Hendryk.Weiland@hhu.de>
2020-06-27 16:06:54 +0200

    chore: add script to init machines at AWS

---
+++ conda_init.sh
@@ -0,0 +1,9 @@
+# Script to set up machines (Linux, x86) at AWS to properly run the playground.
+# Install miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+source .bashrc
+
+# Install dependencies into environment bachelor_playground
+conda create --name bachelor_playground --file conda_specs.txt
+conda activate bachelor_playground
