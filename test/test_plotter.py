fix/refactor: fix loss calculation in training loop, massive improvements of plot-functions and early-stop evaluation, minor improvements

test/test_plotter.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from unittest import main as unittest_main
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from training import plotter
@@ -10,6 +11,44 @@ class TestPlotter(TestCase):
     """ Tests for the plotter module.
     Call with 'python -m test.test_plotter' from project root '~'.
     Call with 'python -m test_plotter' from inside '~/test'. """
+
+    # evaluation
+    def test_find_stop_iteration(self):
+        """ Should find the correct iterations for the early-stopping criterion.
+        The input has shape (1,2,5), thus the result needs to have shape (1,2). """
+        arr = np.array([[[5, 4, 3, 2, 1], [9, 8, 7, 8, 9]]], dtype=float)
+        expected_iterations = np.array([[4, 2]])
+
+        result_iterations = plotter.find_stop_iteration(arr)
+        np.testing.assert_array_equal(expected_iterations, result_iterations)
+
+    def test_get_values_at_stop_iteration(self):
+        """ Should find the corresponding values for the early-stopping indices.
+        The original array has shape (1,2,5) and indices have shape (1,2).
+        Thus the result needs to have shape (1,2,1). """
+        hists = np.array([[[5, 4, 3, 2, 1], [9, 8, 7, 8, 9]]], dtype=float)
+        stop_indices = np.array([[4, 2]])
+
+        expected_values = np.array([[[1], [7]]])
+
+        result_values = plotter.get_values_at_stop_iteration(stop_indices, hists)
+        np.testing.assert_array_equal(expected_values, result_values)
+
+    def test_time_smaller_than_minute_should_be_seconds(self):
+        """ If the given time is smaller than one minute, it should be returned as seconds with four digits. """
+        self.assertEqual("1.2222sec", plotter.format_time(1.22222))
+
+    def test_time_smaller_than_minute_should_be_seconds2(self):
+        """ If the given time is smaller than one minute, it should be returned as seconds with four digits. """
+        self.assertEqual("59.1234sec", plotter.format_time(59.12345))
+
+    def test_time_bigger_than_minute_should_be_minutes_and_seconds(self):
+        """ If the given time is bigger than a minute, it should be returned as minutes and seconds without digits. """
+        self.assertEqual("1:00min", plotter.format_time(60.12345))
+
+    def test_time_bigger_than_minute_should_be_minutes_and_seconds2(self):
+        """ If the given time is bigger than a minute, it should be returned as minutes and seconds without digits. """
+        self.assertEqual("2:18min", plotter.format_time(138.44444))
 
     def test_calculate_correct_mean_and_y_error(self):
         """ Should calculate the correct averages, maxima and minima.
@@ -31,33 +70,87 @@ class TestPlotter(TestCase):
         self.assertTrue((expected_min == result_min).all)
         self.assertTrue((expected_max == result_max).all)
 
-    def test_calculate_correct_early_stop_iteration(self):
-        """ Should calculate the correct iteration for the early stopping criterion.
-        The input has shape (1,2,5), thus the result needs to have shape (1,2). """
-        arr = np.array([[[5, 4, 3, 2, 1], [3, 2, 3, 2, 3]]], dtype=float)
-        expected_iterations = np.array([[4, 1]])
-        expected_shape = (1, 2)
+    # generators
+    def test_generate_iteration_space(self):
+        """ Should generate the correct iteration space. """
+        arr = np.zeros(3)
+        plot_step = 2
 
-        result_iterations = plotter.find_stop_iteration(arr)
+        expected_space = np.array([2, 4, 6])
+        np.testing.assert_array_equal(expected_space, plotter.gen_iteration_space(arr, plot_step))
 
-        self.assertEqual(expected_shape, result_iterations.shape)
-        self.assertTrue((expected_iterations == result_iterations).all)
+    def test_gen_labels_for_train_loss_and_iterations(self):
+        """ Should generate the correct labels. """
+        ax = plt.figure().subplots(1, 1, sharex=False)
+        plotter.gen_labels_on_ax(ax, plotter.PlotType.TRAIN_LOSS, iteration=True)
+        self.assertEqual(ax.get_ylabel(), plotter.PlotType.TRAIN_LOSS)
+        self.assertEqual(ax.get_xlabel(), "Iteration")
 
-    def test_time_smaller_than_minute_should_be_seconds(self):
-        """ If the given time is smaller than one minute, it should be returned as seconds with four digits. """
-        self.assertEqual("1.2222sec", plotter.format_time(1.22222))
+    def test_gen_labels_for_val_acc_and_sparsity(self):
+        """ Should generate the correct labels. """
+        ax = plt.figure().subplots(1, 1, sharex=False)
+        plotter.gen_labels_on_ax(ax, plotter.PlotType.VAL_ACC, iteration=False)
+        self.assertEqual(ax.get_ylabel(), plotter.PlotType.VAL_ACC)
+        self.assertEqual(ax.get_xlabel(), "Sparsity")
 
-    def test_time_smaller_than_minute_should_be_seconds2(self):
-        """ If the given time is smaller than one minute, it should be returned as seconds with four digits. """
-        self.assertEqual("59.1234sec", plotter.format_time(59.12345))
+    def test_gen_title_for_test_acc_on_ax(self):
+        """ Should generate the correct title. """
+        ax = plt.figure().subplots(1, 1, sharex=False)
+        plotter.gen_title_on_ax(ax, 3, plotter.PlotType.TEST_ACC, early_stop=False)
+        self.assertEqual(ax.get_title(), f"Average {plotter.PlotType.TEST_ACC} for 3 pruned Networks")
 
-    def test_time_bigger_than_minute_should_be_minutes_and_seconds(self):
-        """ If the given time is bigger than a minute, it should be returned as minutes and seconds without digits. """
-        self.assertEqual("1:00min", plotter.format_time(60.12345))
+    def test_gen_title_for_test_acc_with_early_stop_on_ax(self):
+        """ Should generate the correct title with early-stop suffix. """
+        ax = plt.figure().subplots(1, 1, sharex=False)
+        plotter.gen_title_on_ax(ax, 3, plotter.PlotType.TEST_ACC, early_stop=True)
+        self.assertEqual(ax.get_title(), f"Average {plotter.PlotType.TEST_ACC} for 3 pruned Networks at early-stop")
 
-    def test_time_bigger_than_minute_should_be_minutes_and_seconds2(self):
-        """ If the given time is bigger than a minute, it should be returned as minutes and seconds without digits. """
-        self.assertEqual("2:18min", plotter.format_time(138.44444))
+    def test_setup_grids_and_y_lim_on_ax(self):
+        """ Should setup grids and set the minimum of y-limit to zero. """
+        ax = plt.figure().subplots(1, 1, sharex=False)
+        ax.set_ylim(bottom=1, top=2)
+        plotter.setup_grids_on_ax(ax, force_zero=True)
+        self.assertEqual(ax.get_ylim()[0], 0)
+
+    def test_setup_grids_on_ax(self):
+        """ Should setup grids and do not change y-limits. """
+        ax = plt.figure().subplots(1, 1, sharex=False)
+        ax.set_ylim(bottom=1, top=2)
+        plotter.setup_grids_on_ax(ax, force_zero=False)
+        self.assertEqual(ax.get_ylim()[0], 1)
+
+    def test_setup_labeling_on_ax(self):
+        """ Should generate the right title and labels, and generate a legend. """
+        plot_type = plotter.PlotType.TEST_ACC
+        net_count = 3
+        ax = plt.figure().subplots(1, 1, sharex=False)
+        ax.plot([0], label='f(x)')  # plot with label to avoid empty legend
+
+        plotter.setup_labeling_on_ax(ax, net_count, plot_type, iteration=False)
+
+        self.assertEqual(ax.get_title(), f"Average {plot_type.value} for {net_count} pruned Networks")
+        self.assertEqual(ax.get_ylabel(), f"{plot_type.value}")
+        self.assertEqual(ax.get_xlabel(), "Sparsity")
+        self.assertIsNotNone(ax.get_legend())
+
+    # plots
+    def test_plot_average_hists(self):
+        """ Should run plot routine without errors. """
+        hists = np.ones((2, 2, 2))
+        sparsity = np.ones(2)
+        plotter.plot_average_hists(hists, sparsity, 10, plotter.PlotType.TRAIN_LOSS)
+
+    def test_plot_two_average_hists(self):
+        """ Should run plot routine without errors. """
+        hists = np.ones((2, 2, 2))
+        sparsity = np.ones(2)
+        plotter.plot_two_average_hists(hists, hists, sparsity, 10, plotter.PlotType.VAL_ACC, plotter.PlotType.VAL_LOSS)
+
+    def test_plot_acc_at_early_stopping(self):
+        """ Should run plot routine without errors. """
+        hists = np.ones((2, 2, 2))
+        sparsity = np.ones(2)
+        plotter.plot_acc_at_early_stopping(hists, hists, sparsity, plotter.PlotType.TEST_ACC)
 
 
 if __name__ == '__main__':

training/plotter.py
@@ -1,23 +1,32 @@
+from enum import Enum
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-# evaluation functions
-def get_means_and_y_errors(arr):
-    """ Calculate means and error bars for given array of arrays.
-    The array is supposed to be of shape (net_count, prune_count+1, data_length).
-    Then this function squashes the net-dimension, so mean, max and min have shape (prune_count+1, data_length). """
-    arr_mean = np.mean(arr, axis=0)
-    arr_neg_y_err = arr_mean - np.min(arr, axis=0)
-    arr_pos_y_err = np.max(arr, axis=0) - arr_mean
-    return arr_mean, arr_neg_y_err, arr_pos_y_err
+class PlotType(str, Enum):
+    """ Defines available types of plots. """
+    TRAIN_LOSS = "Training-Loss"
+    VAL_LOSS = "Validation-Loss"
+    VAL_ACC = "Validation-Accuracy"
+    TEST_ACC = "Test-Accuracy"
 
 
+# evaluation
 def find_stop_iteration(loss_hists):
-    """ Find the minimum for each loss history in 'loss_hists' and return its indices.
-    'loss_hists' is supposed be of shape (net_count, prune_count, iteration_count).
-    The result has shape (net_count, prune_count) and contains the earliest index with minimum loss. """
+    """ Find the early-stop indices for each (validation-)loss-history in 'loss_hists'.
+    Suppose 'loss_hists' has shape (net_count, prune_count, iteration_count).
+    The result has shape (net_count, prune_count) and contains the earliest index with minimum loss along the last axis,
+    saved per net_count and prune_count. """
     return np.argmin(loss_hists, axis=2)
+
+
+def get_values_at_stop_iteration(stop_indices, hists):
+    """ Find the actual values from hists at given stop_indices, e.g. to get the validation loss at these times.
+    Suppose 'stop_indices' has shape (net_count, prune_count) and 'hists' has shape
+    (net_count, prune_count, iteration_count).
+    The result has shape (net_count, prune_count, 1) and contains corresponding values from 'hists' in the last dim. """
+    return np.take_along_axis(hists, np.expand_dims(stop_indices, axis=2), axis=2)
 
 
 def format_time(time):
@@ -30,126 +39,144 @@ def format_time(time):
     return f"{time:.4f}sec"
 
 
-# generator functions for plots
-def gen_baseline_mean_plot_on_ax(ax, xs, ys, y_err_neg, y_err_pos):
+def get_means_and_y_errors(arr):
+    """ Calculate means and error bars for given array of arrays.
+    The array is supposed to be of shape (net_count, prune_count+1, data_length).
+    Then this function squashes the net-dimension, so mean, max and min have shape (prune_count+1, data_length). """
+    arr_mean = np.mean(arr, axis=0)
+    arr_neg_y_err = arr_mean - np.min(arr, axis=0)
+    arr_pos_y_err = np.max(arr, axis=0) - arr_mean
+    return arr_mean, arr_neg_y_err, arr_pos_y_err
+
+
+# generators
+def gen_iteration_space(arr, plot_step):
+    """ Generate a linear space from plot_step with the same length as arr and step size plot_step. """
+    len_arr = len(arr)
+    return np.linspace(start=plot_step, stop=len_arr * plot_step, num=len_arr)
+
+
+def gen_labels_on_ax(ax, plot_type: PlotType, iteration=True):
+    """ Generates labels for the x- and y-axis on given ax.
+    'iteration' defines if x-labels should be generated for iterations or sparsity. """
+    ax.set_ylabel(f"{plot_type.value}")
+    ax.set_xlabel(f"{'Iteration' if iteration else 'Sparsity'}")
+
+
+def gen_title_on_ax(ax, net_count, plot_type: PlotType, early_stop=False):
+    """ Generates plot-title for net_count nets on given ax.
+    'early_stop' defines if early-stopping should be mentioned. """
+    if not early_stop:
+        ax.set_title(f"Average {plot_type.value} for {net_count} pruned Networks")
+    else:
+        ax.set_title(f"Average {plot_type.value} for {net_count} pruned Networks at early-stop")
+
+
+def setup_grids_on_ax(ax, force_zero=False):
+    """ Setup grids for on given ax.
+    force_zero sets the minimum y-value to zero, e.g. for loss plots. """
+    ax.grid()
+    if force_zero:
+        ax.set_ylim(bottom=0)
+
+
+def setup_labeling_on_ax(ax, net_count, plot_type: PlotType, iteration=True):
+    """ Setup complete labeling on ax, i.e. generate title, labels and legend. """
+    gen_title_on_ax(ax, net_count, plot_type)
+    gen_labels_on_ax(ax, plot_type, iteration)
+    ax.legend()
+
+
+# subplots
+def plot_baseline_mean_on_ax(ax, xs, ys, y_err_neg, y_err_pos):
     """ Plots the baseline as dashed line wit error bars on given ax. """
     ax.errorbar(x=xs, y=ys, yerr=[y_err_neg, y_err_pos], label="Mean after 0 prunes", ls='--')
 
 
-def gen_pruned_mean_plots_on_ax(ax, xs, ys, y_err_neg, y_err_pos, sparsity_hist, prune_min, prune_max):
+def plot_pruned_means_on_ax(ax, xs, ys, y_err_neg, y_err_pos, sparsity_hist, prune_min, prune_max):
     """ Plots means per pruning level as line with error bars on given ax.
      Labels contain the sparsity at given level of pruning.
      prune_min and prune_max specify the prune-levels to plot. """
     for p in range(prune_min, prune_max + 1):
         ax.errorbar(x=xs, y=ys[p], yerr=[y_err_neg[p], y_err_pos[p]],
-                    label=f"Mean after {p} prunes (sparsity  {sparsity_hist[p]:.4})")
+                    label=f"Mean after {p} prunes (sparsity {sparsity_hist[p]:.4})")
 
 
-def setup_loss_grids_on_ax(ax):
-    """ Setup grids for loss plots on given ax. """
-    ax.grid('major')
-    ax.set_ylim(bottom=0)
+def plot_averages_on_ax(ax, hists, sparsity_hist, plot_step):
+    """ Plot means and error-bars for given hists on ax.
+    Suppose hists has shape (net_count, prune_count).
+    Plots dashed baseline (unpruned) and a solid line for each pruning step. """
+    _, prune_count, _ = hists.shape
+    prune_count -= 1  # baseline at index 0, thus first pruned round at index 1
+
+    hists_mean, hists_neg_y_err, hists_pos_y_err = get_means_and_y_errors(hists)
+    xs = gen_iteration_space(hists_mean[0], plot_step)
+
+    plot_baseline_mean_on_ax(ax, xs, hists_mean[0], hists_neg_y_err[0], hists_pos_y_err[0])
+    plot_pruned_means_on_ax(ax, xs, hists_mean, hists_neg_y_err, hists_pos_y_err, sparsity_hist, 1, prune_count)
 
 
-def setup_acc_grids_on_ax(ax, force_one=False):
-    """ Setup grids for accuracy plots on given ax. """
-    ax.grid('major')
-    if force_one:
-        ax.set_ylim(top=1)
-
-
-def gen_plot_average_acc_on_ax(ax, acc_hists, sparsity_hist, force_one=False):
-    """ Generate plots of means and error-bars for given accuracies (per epoch) on ax. """
-    net_count, prune_count, epoch_count = acc_hists.shape
-    prune_count -= 1
+def plot_average_acc_at_early_stop_on_ax(ax, acc_hists, sparsity_hist):
+    """ Plot means and error-bars for given accuracies (per sparsity) on ax. """
+    # each mean has shape (prune_count+1, 1), so squeeze them to shape (prune_count+1)
     acc_mean, acc_neg_y_err, acc_pos_y_err = get_means_and_y_errors(acc_hists)
-    acc_xs = np.linspace(start=1, stop=epoch_count, num=epoch_count)
+    acc_mean = np.squeeze(acc_mean)
+    acc_neg_y_err = np.squeeze(acc_neg_y_err)
+    acc_pos_y_err = np.squeeze(acc_pos_y_err)
 
-    gen_baseline_mean_plot_on_ax(ax, acc_xs, acc_mean[0], acc_neg_y_err[0], acc_pos_y_err[0])
-    gen_pruned_mean_plots_on_ax(ax, acc_xs, acc_mean, acc_neg_y_err, acc_pos_y_err, sparsity_hist, 1, prune_count)
-    setup_acc_grids_on_ax(ax, force_one)
-
-
-def gen_plot_average_loss_on_ax(ax, loss_hists, sparsity_hist, plot_step):
-    """ Generate plots of means and error-bars for given losses on ax. """
-    net_count, prune_count, _ = loss_hists.shape
-    prune_count -= 1
-    loss_mean, loss_neg_y_err, loss_pos_y_err = get_means_and_y_errors(loss_hists)
-    loss_xs = np.linspace(start=plot_step, stop=len(loss_mean[0]) * plot_step, num=len(loss_mean[0]))
-
-    gen_baseline_mean_plot_on_ax(ax, loss_xs, loss_mean[0], loss_neg_y_err[0], loss_pos_y_err[0])
-    gen_pruned_mean_plots_on_ax(ax, loss_xs, loss_mean, loss_neg_y_err, loss_pos_y_err, sparsity_hist, 1, prune_count)
-    setup_loss_grids_on_ax(ax)
+    # plot
+    ax.errorbar(x=sparsity_hist, y=acc_mean, yerr=[acc_neg_y_err, acc_pos_y_err], label="Accuracy", marker='x')
+    ax.set_xticks(sparsity_hist)
+    ax.invert_xaxis()  # also inverts plot!
 
 
-def gen_plot_average_acc_at_early_stop_on_ax(ax, acc_hists, sparsity_hist, force_one=False):
-    """ Generate plots of means and error-bars for given accuracies (per sparsity) on ax. """
-    acc_mean, acc_neg_y_err, acc_pos_y_err = get_means_and_y_errors(acc_hists)  # each result has shape (prune_count)
-
-    # plot baseline, i.e. unpruned results
-    ax.errorbar(x=sparsity_hist, y=acc_mean, yerr=[acc_neg_y_err, acc_pos_y_err], label="Mean of accs", marker='x')
-    ax.invert_xaxis()
-    setup_acc_grids_on_ax(ax, force_one)
-
-
-def gen_labels_for_acc_on_ax(ax, test=True, epoch=True):
-    """ Generate standard labels for validation-plots on given ax.
-    'test' defines if y-labels should be generated for test- or validation-accuracies.
-    'epoch' defines if x-labels should be generated for epochs or sparsity. """
-    ax.set_ylabel(f"{'Test' if test else 'Validation'}-Accuracy")
-    ax.set_xlabel(f"{'Epoch' if epoch else 'Sparsity'}")
-    ax.legend()
-
-
-def gen_labels_for_loss_on_ax(ax):
-    """ Generate standard labels for loss-plots on given ax. """
-    ax.set_ylabel("Loss")
-    ax.set_xlabel("Iteration")
-    ax.legend()
-
-
-# plotting functions
-def plot_average_acc(acc_hists, sparsity_hist, test=True):
-    """ Plot means and error bars for the given accuracies.
+# plots
+def plot_average_hists(hists, sparsity_hist, plot_step, plot_type: PlotType):
+    """ Plot means and error bars for the given histories in hists.
     'test' defines if labels should be generated for test- or validation-accuracies. """
     # setup and plot
     fig = plt.figure(None, (7, 6))
     ax = fig.subplots(1, 1, sharex=False)
-    gen_plot_average_acc_on_ax(ax, acc_hists, sparsity_hist)
+    plot_averages_on_ax(ax, hists, sparsity_hist, plot_step)
+    setup_grids_on_ax(ax)
 
     # labeling
-    net_count, prune_count, _ = acc_hists.shape
-    ax.set_title(f"Average {'Test' if test else 'Validation'}-Accuracy for {net_count} pruned Networks")
-    gen_labels_for_acc_on_ax(ax, test)
+    net_count, _, _ = hists.shape
+    setup_labeling_on_ax(ax, net_count, plot_type)
 
 
-def plot_average_val_acc_and_loss(acc_hists, loss_hists, sparsity_hist, plot_step):
-    """ Plot means and error bars for validation-accuracies and loss. """
+def plot_two_average_hists(hists_left, hists_right, sparsity_hist, plot_step, type_left: PlotType, type_right: PlotType,
+                           force_zero_left=False, force_zero_right=False):
+    """ Plot means and error bars for two hists side by side. """
     # setup and plot
     fig = plt.figure(None, (14, 6))
-    ax_acc, ax_loss = fig.subplots(1, 2, sharex=False)
-    gen_plot_average_acc_on_ax(ax_acc, acc_hists, sparsity_hist)
-    gen_plot_average_loss_on_ax(ax_loss, loss_hists, sparsity_hist, plot_step)
+    ax_left, ax_right = fig.subplots(1, 2, sharex=False)
+    plot_averages_on_ax(ax_left, hists_left, sparsity_hist, plot_step)
+    plot_averages_on_ax(ax_right, hists_right, sparsity_hist, plot_step)
+    setup_grids_on_ax(ax_left, force_zero_left)
+    setup_grids_on_ax(ax_right, force_zero_right)
 
     # labeling
-    net_count, prune_count, _ = acc_hists.shape
-    ax_acc.set_title(f"Average Validation-Accuracy for {net_count} pruned Networks")
-    ax_loss.set_title(f"Average Loss for {net_count} pruned Networks")
-    gen_labels_for_acc_on_ax(ax_acc, test=False)
-    gen_labels_for_loss_on_ax(ax_loss)
+    net_count, _, _ = hists_left.shape
+    setup_labeling_on_ax(ax_left, net_count, type_left)
+    setup_labeling_on_ax(ax_right, net_count, type_right)
 
 
-def plot_acc_at_early_stopping(acc_hists, loss_hists, sparsity_hist, test=True):
+def plot_acc_at_early_stopping(acc_hists, loss_hists, sparsity_hist, plot_type: PlotType):
     """ Plot means and error bars for the given accuracies at the time an early stopping criterion would end training.
-    The x-axis shows the sparsity at each time..
-    'test' defines if labels should be generated for test- or validation-accuracies. """
+    The x-axis shows the sparsity at each time.
+    'test_acc' defines if labels should be generated for test- or validation-accuracies. """
+    # apply early-stopping criterion on loss_hists to find corresponding accuracies
+    early_stop_indices = find_stop_iteration(loss_hists)
+    early_stop_acc = get_values_at_stop_iteration(early_stop_indices, acc_hists)
+
+    # plot
     fig = plt.figure(None, (7, 6))
     ax = fig.subplots(1, 1, sharex=False)
-    early_stop_indices = find_stop_iteration(loss_hists)
-    gen_plot_average_acc_at_early_stop_on_ax(ax, acc_hists[-1, -1, early_stop_indices], sparsity_hist, force_one=False)
+    plot_average_acc_at_early_stop_on_ax(ax, early_stop_acc, sparsity_hist)
+    setup_grids_on_ax(ax)
 
     # labeling
-    net_count, prune_count, _ = acc_hists.shape
-    ax.set_title(
-        f"Average {'Test' if test else 'Validation'}-Accuracy for {net_count} pruned Networks at early stopping")
-    gen_labels_for_acc_on_ax(ax, test, epoch=False)
+    net_count, _, _ = acc_hists.shape
+    gen_title_on_ax(ax, net_count, plot_type, early_stop=True)
+    gen_labels_on_ax(ax, plot_type, iteration=False)

training/trainer.py
@@ -3,7 +3,6 @@ import time
 
 import numpy as np
 import torch
-import torch.optim as optim
 
 from experiments.experiment_settings import VerbosityLevel
 from training import plotter
@@ -48,7 +47,7 @@ class TrainerAdam(object):
         test_acc_hist = np.zeros_like(train_loss_hist, dtype=float)
 
         # setup training
-        opt = optim.Adam(net.parameters(), lr=self.learning_rate)  # instantiate optimizer
+        opt = torch.optim.Adam(net.parameters(), lr=self.learning_rate)  # instantiate optimizer
         running_train_loss = 0
         hist_count = 0
 
@@ -78,12 +77,13 @@ class TrainerAdam(object):
                     test_acc_hist[hist_count] = self.compute_acc(net, test=True)
 
                     hist_count += 1
+                    running_train_loss = 0
                     net.train(True)  # set model to training mode (important for batch-norm/dropout)
                     log_detailed_only(self.verbosity, f"-", False)
 
             toc = time.time()
             log_from_medium(self.verbosity,
-                            f"val-acc: {(val_acc_hist[hist_count - 1]):1.4} (took {plotter.format_time(toc - tic)})\n")
+                            f"val-acc: {(val_acc_hist[hist_count - 1]):1.4} (took {plotter.format_time(toc - tic)})")
         return net, train_loss_hist, val_loss_hist, val_acc_hist, test_acc_hist
 
     def compute_acc(self, net, test=True):