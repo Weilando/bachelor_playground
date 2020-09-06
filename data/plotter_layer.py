27373603987bff262b80507403b5d62d080d7a6f
Hendryk Weiland <35635965+Weilando@users.noreply.github.com>
2020-09-06 17:37:50 +0200

    Feat/histogram visualization (#11)
    feat: add histogram next to color-bar

--- data/plotter_layer.py
+++ data/plotter_layer.py
@@ -3,6 +3,7 @@ import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 from math import ceil
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from torch import nn
 from torch.nn.utils import prune
 
@@ -18,7 +19,8 @@ def get_cmap():
 
 def plot_kernels(conv_2d, num_cols=8):
     """ Plot the weights of all kernels from 'conv_2d' as rectangles on a new figure and map values to colors.
-    Create one normalized image per channel and kernel to avoid clipping and to center all values at zero. """
+    Create one normalized image per channel and kernel to avoid clipping and to center all values at zero.
+    Show a colorbar with integrated histogram on the right. """
     assert isinstance(conv_2d, nn.Conv2d), f"'conv_2d' has invalid type {type(conv_2d)}"
 
     weights = conv_2d.weight.data.clone().numpy()  # shape is [kernels, channels, height, width]
@@ -32,16 +34,31 @@ def plot_kernels(conv_2d, num_cols=8):
     weight_norm = plotter_evaluation.get_norm_for_sequential(nn.Sequential(conv_2d))
     num_cols, num_rows = plotter_evaluation.get_row_and_col_num(weights.shape, num_cols)
 
-    fig = plt.figure(figsize=(num_cols, num_rows))
+    fig = plt.figure(figsize=(num_cols + 1, num_rows), constrained_layout=False)
+    gs = fig.add_gridspec(num_rows, num_cols + 1, wspace=0.1, hspace=0.4)  # extra column for colorbar and histogram
     for kernel_counter, kernel in enumerate(weights[:]):
-        for channel_counter, channel in enumerate(kernel[:], 1):
-            ax = fig.add_subplot(num_rows, num_cols, kernel_counter * kernel.shape[0] + channel_counter)
+        for channel_counter, channel in enumerate(kernel[:]):
+            ax_counter = kernel_counter * kernel.shape[0] + channel_counter
+            ax = fig.add_subplot(gs[ax_counter // num_cols, ax_counter % num_cols])
             ax.imshow(channel, cmap=get_cmap(), norm=weight_norm)
-            ax.set_title(f"K{kernel_counter + 1}.{channel_counter}").set_position([.5, 0.95])
+            ax.set_title(f"K{kernel_counter + 1}.{channel_counter + 1}").set_position([.5, 0.95])
             ax.axis('off')
 
-    fig.colorbar(fig.axes[0].images[0], ax=fig.axes, fraction=0.1)
-    fig.subplots_adjust(wspace=0.1, hspace=0.1, right=0.75)
+    # append two columns for colorbar and histogram
+    tmp_ax = fig.add_subplot(gs[:, -1])
+    tmp_ax.axis('off')
+    div = make_axes_locatable(tmp_ax)
+    cax = div.append_axes("right", size="25%", pad=0)
+    hax = div.append_axes("right", size="75%", pad=0)
+
+    fig.colorbar(fig.axes[0].images[0], cax=cax, aspect=10)
+    cax.yaxis.set_ticks_position('left')
+
+    hax.hist(weights.flatten(), orientation='horizontal', density=False, bins=30, color='gray')
+    hax.yaxis.set_visible(False)
+    for loc in ['top', 'right', 'left']:
+        hax.spines[loc].set_visible(False)
+
     return fig
 
 
